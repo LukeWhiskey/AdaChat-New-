89d1f727613cd0045e330ba63d97ca567714c25c
Luke Obornik <93653855+LukeWhiskey@users.noreply.github.com>
2022-11-19 02:31:43 +0000

    Delete AdaChat directory

--- AdaChat/AdaChat.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("AdaH").innerHTML = "AdaChat";
-document.getElementById("AdaS").innerHTML = "C h a t F r e e l y ! ! !";
-document.onscroll = function () {
-    document.getElementById("body").style.backgroundPositionY = document.scrollingElement.scrollTop * 0.2 + "px";
-    document.getElementById("body2").style.backgroundPositionY = document.scrollingElement.scrollTop * 0.5 + "px";
-}
